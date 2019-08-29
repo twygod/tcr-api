ee0344b6a5c7b912f33a2f1444e75115c4261551
twygod <twygod@gmail.com>
2019-08-29 19:29:43 +0800

    Revert "tag修改"
    This reverts commit 3fa95c5ab8b5fc52051b8e74c2476212919313bb.

--- test/registry.spec.js
+++ test/registry.spec.js
@@ -1,34 +1,34 @@
-import { Listing, PLCRVoting } from '../lib/tcr';
-import Registry from '../src/components/Registry.js'
-import Web3 from 'web3';
+// import { Registry, Listing, PLCRVoting } from '../lib/tcr';
+// import Web3 from 'web3';
 
-const assert = require('assert');
-const secrets = require('../src/secrets.json');
-let registryAddress = secrets.registry;
-let provider = new Web3('http://localhost:8545');
+// const assert = require('assert');
+// const secrets = require('../src/secrets.json');
 
-let registry, account, accountExtra, parameterizer, accounts;
+// let registryAddress = secrets.registry;
+// let provider = new Web3('http://localhost:7545');
 
-const sleep = (seconds) => (new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000)));
+// let registry, account, accountExtra, parameterizer, accounts;
 
-const getRandInt = (min, max) => (Math.floor(Math.random() * (max - min)) + min);
+// const sleep = (seconds) => (new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000)));
 
-describe('TCR', () => {
-  let listingHash;
+// const getRandInt = (min, max) => (Math.floor(Math.random() * (max - min)) + min);
 
-  before(async () => {
-    // Settings the default address in case it's empty
-    accounts = await provider.eth.getAccounts();
-    provider.eth.defaultAccount = accounts[0];
+// describe('TCR', () => {
+//   let listingHash;
 
-    // Settings the default entities
-    registry = new Registry(registryAddress, provider);
-    account = await registry.getAccount(await provider.eth.getCoinbase());
-    accountExtra = await registry.getAccount(accounts[1]);
-    parameterizer = await registry.getParameterizer();
+//   before(async () => {
+//     // Settings the default address in case it's empty
+//     accounts = await provider.eth.getAccounts();
+//     provider.eth.defaultAccount = accounts[0];
 
-    listingHash = Listing.hashName(Math.random(10000).toString()); // Pseudo random listing name to avoid collisions
-  });
+//     // Settings the default entities
+//     registry = new Registry(registryAddress, provider);
+//     account = await registry.getAccount(await provider.eth.getCoinbase());
+//     accountExtra = await registry.getAccount(accounts[1]);
+//     parameterizer = await registry.getParameterizer();
+
+//     listingHash = Listing.hashName(Math.random(10000).toString()); // Pseudo random listing name to avoid collisions
+//   });
 
 //   describe('Account', () => {
 //     before(async () => {
@@ -49,70 +49,70 @@ describe('TCR', () => {
 //     });
 //   });
 
-  describe('Registry', () => {
-    let minDeposit, stake, depositAmount;
+//   describe('Registry', () => {
+//     let minDeposit, stake, depositAmount;
 
-    before(async () => {
-      let amount = 0;
+//     before(async () => {
+//       let amount = 0;
 
-      /* Calculating tokens amount for applying pre-approving */
-      minDeposit = parseInt(await parameterizer.get('minDeposit'), 10);
-      let addition = minDeposit * Number(Math.random(0.2).toFixed()); // Addition from 0% to 20% to minDeposit
+//       /* Calculating tokens amount for applying pre-approving */
+//       minDeposit = parseInt(await parameterizer.get('minDeposit'), 10);
+//       let addition = minDeposit * Number(Math.random(0.2).toFixed()); // Addition from 0% to 20% to minDeposit
 
-      stake = minDeposit + addition;
-      amount += stake;
+//       stake = minDeposit + addition;
+//       amount += stake;
 
-      /* Calculating tokens amount for deposit pre-approving */
-      depositAmount = 10000;
-      amount += depositAmount;
+//       /* Calculating tokens amount for deposit pre-approving */
+//       depositAmount = 10000;
+//       amount += depositAmount;
 
-      account.approveTokens(registry.address, amount);
+//       account.approveTokens(registry.address, amount);
 
-      /* Approving min deposit tokens to extra account */
-      accountExtra.approveTokens(registry.address, minDeposit * 10);
-    });
+//       /* Approving min deposit tokens to extra account */
+//       accountExtra.approveTokens(registry.address, minDeposit * 10);
+//     });
 
-    // it('should be able to create a listing', async () => {
-    //   let listing = await registry.createListing(listingHash, stake, 'wow data', {gas: 150000});
+//     it('should be able to create a listing', async () => {
+//       let listing = await registry.createListing(listingHash, stake, 'wow data', {gas: 150000});
 
-    //   assert(await registry.hasListing(listingHash));
-    //   assert.strictEqual(await listing.getStageStatus(), null);
-    // });
+//       assert(await registry.hasListing(listingHash));
+//       assert.strictEqual(await listing.getStageStatus(), null);
+//     });
 
-    // it('should be able to increase and decrease listing deposit', async () => {
-    //   let listing = registry.getListing(listingHash);
-    //   let deposit = await listing.getDeposit();
+//     it('should be able to increase and decrease listing deposit', async () => {
+//       let listing = registry.getListing(listingHash);
+//       let deposit = await listing.getDeposit();
 
-    //   await listing.deposit(depositAmount);
-    //   assert.strictEqual(await listing.getDeposit(), deposit + depositAmount);
+//       await listing.deposit(depositAmount);
+//       assert.strictEqual(await listing.getDeposit(), deposit + depositAmount);
 
-    //   await listing.withdraw(depositAmount);
-    //   assert.strictEqual(await listing.getDeposit(), deposit);
-    // });
+//       await listing.withdraw(depositAmount);
+//       assert.strictEqual(await listing.getDeposit(), deposit);
+//     });
 
-    // it('should be able to challenge listing', async () => {
-    //   let listing = registry.getListing(listingHash);
+//     it('should be able to challenge listing', async () => {
+//       let listing = registry.getListing(listingHash);
 
-    //   assert(!await listing.hasChallenge());
+//       assert(!await listing.hasChallenge());
 
-    //   await listing.challenge({from: accountExtra.owner, gas: 400000});
+//       await listing.challenge({from: accountExtra.owner, gas: 400000});
 
-    //   assert(await listing.hasChallenge());
-    // });
+//       assert(await listing.hasChallenge());
+//     });
 
-    // @todo: not ready yet, need to implement challenge functionality
-    // it('should be able to remove listing (quit from registry)', async () => {
-    //   return;
-    //   let listing = registry.getListing(listingHash);
+//     // @todo: not ready yet, need to implement challenge functionality
+//     it('should be able to remove listing (quit from registry)', async () => {
+//       return;
+//       let listing = registry.getListing(listingHash);
 
-    //   // Checking that the listing is in registry and whitelisted
-    //   assert(await listing.exists());
-    //   assert(await listing.isWhitelisted());
+//       // Checking that the listing is in registry and whitelisted
+//       assert(await listing.exists());
+//       assert(await listing.isWhitelisted());
 
-    //   await listing.remove();
-    //   assert(!await listing.exists());
-    // });
-  });
+//       await listing.remove();
+//       assert(!await listing.exists());
+//     });
+//   });
 
 //   describe('PLCR Voting', () => {
 //     let listing, challenge, plcr, salt, option, depositAmount;
@@ -286,4 +286,4 @@ describe('TCR', () => {
 //       await challenge.claimReward(salt, {from: accountExtra.owner, gas: 150000});
 //     });
 //   });
-});
+// });
